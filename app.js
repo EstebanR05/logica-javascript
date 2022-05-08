be097920fc0baee1165ccdd673627b67bc265df0
EstebanR05 <erestrepo@webcol.net>
2022-05-08 13:30:23 -0500

    sumar, restar, multiplicar y dividir
    se aprendio

---
+++ app.js
@@ -0,0 +1,41 @@
+//sumar
+/*
+let num1= 3;
+let numb2= 6;
+
+let suma= num1 + numb2;
+
+console.log(suma);
+*/
+
+//restar
+/*
+let num1= 3;
+let numb2= 6;
+
+let resta= num1 - numb2;
+
+console.log(resta);
+*/
+
+//multiplicar
+/*
+let num1= 3;
+let numb2= 6;
+
+let multiplicar= num1 * numb2;
+
+console.log(multiplicar);
+*/
+
+//dividir
+/*
+let num1= 3;
+let numb2= 6;
+
+let suma= num1 / numb2;
+
+console.log(suma);
+*/
+
+//operadores logicos
