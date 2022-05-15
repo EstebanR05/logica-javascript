82dd02b1f50ad5930e7226696ac756ec999a2857
EstebanR05 <erestrepo@webcol.net>
2022-05-14 22:53:40 -0500

    mas logica
    ya casi se alcanza el objetivo

--- app.js
+++ app.js
@@ -37,5 +37,128 @@ let suma= num1 / numb2;
 
 console.log(suma);
 */
+//concatenar normal
+/*
+let num1 = 3;
+let num2 = 10;
 
-//operadores logicos
+concatenar = "" + num1 + num2;
+
+document.write(concatenar);
+*/
+
+//concatenar con concat
+/*
+let num = "3";
+let num2 = 5;
+
+concatenar = num.concat(num2);
+document.write(concatenar);
+*/
+
+//concatenar sin blacktikes
+/*
+let nombre;
+nombre = prompt("Escribe tu nombre");
+Frase = "Hola señor" + nombre + "desea continuar ?";
+
+document.write(Frase);
+*/
+
+//concatenar con blacktikes
+/*
+let nombre;
+nombre = prompt("Escribe tu nombre");
+Frase = `Hola señor ${nombre} desea continuar?`;
+
+document.write(Frase);
+*/
+
+//camelcase
+/*
+document.writeById();
+
+*/
+
+//condicional if, else
+/*
+let num1 = 3;
+let num2 = 6;
+
+if (num1 <= num2 && num1 !== num) {
+    console.log("respuesta 1");
+} else {
+    console.log("respuesta 2");
+}
+*/
+
+//if, else if and else
+/*
+let num1 = 3;
+let num2 = 6;
+
+if (num1 >= num2) {
+    console.log("respuesta 1");
+} else if (num1 != num2) {
+    console.log("respuesta 2");
+} else {
+    console.log("respuesta 3");
+}*/
+
+//while, do while
+/*
+var i = 1;
+
+while (i <= 10) {
+    i++;
+    console.log(i);
+}
+*/
+
+//do while
+/*
+do {
+    console.log(i);
+    i++;
+} while (i <= 10);
+*/
+
+//sentencia for
+
+let exp1 = 1;
+
+for (exp1; exp1 <= 5; exp1++) {
+    if (exp1 == 4) {
+        continue;
+    }
+    document.write(exp1 + "agregar al diccionario");
+}
+
+//for in 
+
+//for on
+
+//sentencia switch
+/*
+let mes = prompt("en que mes estas?");
+
+switch (mes) {
+    case 1:
+        console.log(`enero`);
+        break;
+    case 2:
+        console.log(`febrero`);
+        break;
+    case 3:
+        console.log(`marzo`);
+        break;
+    case 4:
+        console.log(`abril`);
+        break;
+    case 5:
+        console.log(`mayo`);
+        break;
+
+    default:
+        console.log("mes no encontrado")
+}*/
